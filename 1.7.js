236ca40a76ee26b54b1ff0a00f88bf1a10083b6e
Amandine Gaullet <am.gaullet@gmail.com>
2022-02-17 10:48:17 +0100

    1.7

---
+++ 1.7.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+let num1 = readlineSync.question("give me your shoe size")
+let num2 = readlineSync.question("give me your date of birth")
+console.log((num1 * 2 ) + 5 * 50 - num2 + 1766);
